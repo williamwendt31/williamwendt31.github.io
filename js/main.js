fa6b7c9674bd28f63a31f652cb68276c7a2f707f
williamwendt31 <williamwendt31@yahoo.com>
2019-01-21 20:03:03 -0800

    new changes

--- js/main.js
+++ js/main.js
@@ -1,3 +1,10 @@
 $(document).ready(() => {
     $('.scrollspy').scrollSpy();
+    $('.pushpin').pushpin();
+    $('.project-tech').hide();
+    $('.project').hover(function() {
+        $(this).find('.project-tech').show();
+    }, function() {
+        $(this).find('.project-tech').hide();
+    });
 });
